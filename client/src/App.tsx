e2e811945c4fa959e09ea6c9a0b908b483a217e8
ymdarake <sakamoto.takumi.dev@gmail.com>
2021-08-26 17:05:01 +0900

    [init]Setup server, worker, client

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">OtherPage</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib}></Route>
+          <Route path="/otherpage" component={OtherPage}></Route>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
